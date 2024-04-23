207156d295920175f534529d9c859e1a400a02be
Arkueid <thetardis@qq.com>
2024-04-23 14:26:20 +0800

    引言、总览、功能

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,36 @@
+export default {
+  title: 'DeskPal',
+  description: 'DeskPal 说明文档',
+  themeConfig: {
+    nav: [
+      // { text: 'Guide', link: '/guide' },
+    ],
+    sidebar: [
+      {
+        text: '开发文档',
+        collapsable: true,
+        items: [
+          {
+            text: '需求分析',
+            items: [
+              { text: '引言', link: '/开发文档/需求分析/引言' },
+              { text: '总览', link: '/开发文档/需求分析/总览' },
+              { text: '功能', link: '/开发文档/需求分析/功能' },
+              { text: '外部接口', link: '/开发文档/需求分析/外部接口' },
+              { text: '非功能性需求', link: '/开发文档/需求分析/非功能性需求' },
+              { text: '其他', link: '/开发文档/需求分析/其他需求' },
+              { text: '附录', link: '/开发文档/需求分析/附录' },
+            ]
+          },
+        ]
+      },
+      {
+        text: '使用说明',
+        items: [
+          { text: '介绍', link: '/使用说明/index' },
+        ]
+      }
+    ]
+  },
+  base: "/Live2DMascot-Docs/"
+}
--- docs/.vuepress/.temp/internal/routes.js
+++
@@ -1,6 +0,0 @@
-export const redirects = JSON.parse("{}")
-
-export const routes = Object.fromEntries([
-  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/forDesktop/DesktopLive2d-Docs/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Live2D Desktop Mascot"} }],
-  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/forDesktop/DesktopLive2d-Docs/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
-]);
